e7c4fc156f26221d9ed20c7c00297853956d8f9d
aki33524 <aki33524@10.4.167.73>
2014-10-08 21:44:03 +0900

    template追加

--- src/template.cpp
+++ src/template.cpp
@@ -3,8 +3,16 @@
 #include <vector>
 #include <queue>
 #include <numeric>
+#include <string>
 
 //ios::sync_with_stdio(false);
 
 using namespace std;
 
+const int INF = (1<<30) - 1;
+
+
+int main(){
+
+	return 0;
+}
