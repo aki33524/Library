0f7d11cd91713dfa464a54cd1d048497f20d1ac6
aki33524 <aki33524@192.168.11.3>
2014-10-14 03:43:05 +0900

    dijkstra

---
+++ src/dijkstra.cpp
@@ -0,0 +1,29 @@
+#include "template.cpp"
+
+typedef struct{
+    int to, cost;
+} Edge;
+
+vector<int> dijkstra(vector<vector<Edge> >& G, int s){
+    int N = G.size();
+    vector<int> d(N, INF);
+
+    priority_queue<pair<int, int> > q;
+    d[s] = 0;
+    q.push(make_pair(d[s], s));
+
+    while(!q.empty()){
+        pair<int, int> v = q.top(); q.pop();
+        if(v.first != d[v.second])
+            continue;
+
+        for(int i=0; i<G[v.second].size(); i++){
+            Edge e = G[v.second][i];
+            if(e.cost + d[v.second] < d[e.to]){
+                d[e.to] = e.cost + d[v.second];
+                q.push(make_pair(d[e.to], e.to));
+            }
+        }
+    }
+    return d;
+}
