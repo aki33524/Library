65eff497e33e50cbeb1136061c5aa24dfed721af
aki33524 <aki33524@192.168.11.3>
2014-10-13 21:09:49 +0900

    ford-fulkersonのクラスバージョン

---
+++ src/ford_fulkerson.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+using namespace std;
+
+const int INF = (1<<30) - 1;
+
+class FordFulkerson{
+private:
+	typedef struct {
+		int to, cap, rev;
+	} Edge;
+
+	vector<vector<Edge> > G;
+	vector<bool> used;
+
+public:
+	FordFulkerson(int n){
+		G.resize(n);
+		used.resize(n);
+	}
+
+	void add_edge(int from, int to, int cap){
+		G[from].push_back((Edge){to, cap, G[to].size()});
+		G[to].push_back((Edge){from, 0, G[from].size()-1});
+	}
+
+	int dfs(int v, int t, int f){
+		if(v == t)
+			return f;
+		used[v] = true;
+		for(int i=0; i<G[v].size(); i++){
+			Edge& e = G[v][i];
+			if(!used[e.to] && e.cap > 0){
+				int d = dfs(e.to, t, min(f, e.cap));
+				if(d > 0){
+					e.cap -= d;
+					G[e.to][e.rev].cap += d;
+					return d;
+				}
+			}
+		}
+		return 0;
+	}
+
+	int max_flow(int s, int t){
+		int res = 0;
+		while(true){
+			fill(used.begin(), used.end(), false);
+			int f = dfs(s, t, INF);
+			if(f == 0)
+				return res;
+			res += f;
+		}
+	}
+};
