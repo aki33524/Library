57411b4de389188de0dc9cea902ef410639c22d7
aki33524 <aki33524@10.4.166.209>
2014-10-15 22:59:59 +0900

    バグを埋める奴はカス

--- src/bellman_ford.cpp
+++ src/bellman_ford.cpp
@@ -14,13 +14,17 @@ bool has_negative_cycle(int n, vector<Edge> edges, int s){
 	d[s] = 0;
 	int i;
 	for(i=0; i<n; i++){
+		bool updated = false;
 		for(int j=0; j<edges.size(); j++){
 			Edge& edge = edges[j];
 			if(d[edge.from]+edge.weight < d[edge.to]){
 				d[edge.to] = d[edge.from]+edge.weight;
 				//par[edge.to] = edge.from;
+				updated = true;
 			}
 		}
+		if(!updated)
+			break;
 	}
 	return i == n-1;
 }
