c3095d19cc9b37a872162b72e49425829d03e010
aki33524 <aki33524@192.168.11.3>
2014-10-17 01:12:29 +0900

    topological sortにdag checkを入れた

--- src/topological_sort.cpp
+++ src/topological_sort.cpp
@@ -1,21 +1,36 @@
 #include "template.cpp"
 
-void dfs(vector<vector<int> >& G, int u, vector<int>& res, vector<bool>& used){
-	for(int i=0; i<G[u].size(); i++){
-		int v = G[u][i];
-		if(!used[v])
-			dfs(G, v, res, used);
-	}
-	res.push_back(u);
-	used[u] = true;
-}
+class TopologicalSort{
+private:
+	enum Color{WHITE, GRAY, BLACK};
+	vector<Color> colors;
 
-vector<int> topological_sort(vector<vector<int> >& G){
-	vector<int> res;
-	vector<bool> used(G.size(), false);
-	for(int v = 0; v < G.size(); v++)
-		if(!used[v])
-			dfs(G, v, res, used);
-	reverse(res.begin(), res.end());
-	return res;
-}
+	void dfs(vector<vector<int> >& G, int u, vector<int>& res){
+		colors[u] = GRAY;
+		for(int i=0; i<G[u].size(); i++){
+			int v = G[u][i];
+			if(colors[v] == GRAY)
+				is_dag = false;
+			if(colors[v] == WHITE)
+				dfs(G, v, res);
+		}
+		res.push_back(u);
+		colors[u] = BLACK;
+	}
+public:
+	bool is_dag;
+
+	vector<int> topological_sort(vector<vector<int> >& G){
+		vector<int> res;
+		colors.resize(G.size());
+		fill(colors.begin(), colors.end(), WHITE);
+		is_dag = true;
+		for(int v = 0; v < G.size(); v++)
+			if(colors[v] == WHITE)
+				dfs(G, v, res);
+		reverse(res.begin(), res.end());
+		return res;
+	}
+};
+
+
