56cd43214e6bfb9c6180b7dcc59c14dcb6936346
aki33524 <aki33524@192.168.11.3>
2014-11-27 00:46:04 +0900

    abs(complex<double>)は良くない http://stackoverflow.com/questions/11108587/why-does-abscomplexint-always-return-zero

--- src/graham_scan.cpp
+++ src/graham_scan.cpp
@@ -1,18 +1,18 @@
 #include "template.cpp"
 
 
-int det(complex<int> l, complex<int> r){
+int det(complex<double> l, complex<double> r){
     return l.real()*r.imag() - l.imag()*r.real();
 }
 
-bool cmp1(const complex<int>& l, const complex<int>& r){
+bool cmp1(const complex<double>& l, const complex<double>& r){
     if(l.imag() == r.imag())
         return l.real() < r.real();
     else
         return l.imag() < r.imag();
 }
 
-bool cmp2(const complex<int>& l, const complex<int>& r){
+bool cmp2(const complex<double>& l, const complex<double>& r){
     int a = det(l, r);
     if(a == 0)
         return abs(l) < abs(r);
@@ -20,16 +20,16 @@ bool cmp2(const complex<int>& l, const complex<int>& r){
         return a > 0;
 }
 
-vector<complex<int> > graham_scan(vector<complex<int> > v){
+vector<complex<double> > graham_scan(vector<complex<double> > v){
     sort(v.begin(), v.end(), cmp1);
 
-    complex<int> p0 = v[0];
+    complex<double> p0 = v[0];
     for(int i=0; i<v.size(); i++)
         v[i] -= p0;
 
     sort(v.begin(), v.end(), cmp2);
 
-    vector<complex<int> > gs(v.size());
+    vector<complex<double> > gs(v.size());
     int k = 0;
     for(int i=0; i<v.size(); i++){
         while(k > 1 && det(gs[k-1]-gs[k-2], v[i]-gs[k-1]) <= 0) k--;
