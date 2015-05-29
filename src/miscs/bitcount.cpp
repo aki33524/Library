d3923018761cdd346b7de8987bebafecb0128933
aki33524 <aki33524@aki33524-no-MacBook-Air.local>
2015-05-29 22:05:16 +0900

    bitcount

---
+++ src/miscs/bitcount.cpp
@@ -0,0 +1,12 @@
+#ifndef _MSC_VER
+static inline int32_t __attribute__((always_inline))
+mm_popcnt_u32(uint32_t x){
+    int32_t result;
+    __asm__("popcnt %1, %0" : "=r" (result) : "r" (x));
+    return result;
+}
+#else
+#	define mm_popcnt_u32 _mm_popcnt_u32
+#endif
+
+//-msse4.2 __builtin_popcount
