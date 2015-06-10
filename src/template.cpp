5b0ce82588663bb300aba16a8c7d04e8e66fce51
aki33524 <aki33524@aki33524-no-MacBook-Air.local>
2015-06-11 07:46:26 +0900

    #5 某から丸々コピー

--- src/template.cpp
+++ src/template.cpp
@@ -12,12 +12,26 @@
 #include <complex>
 #include <stack>
 #include <tuple>
-
-#define ll long long int
-
 using namespace std;
+#define rep(i,n) for (int i=0; i < (n); i++)
+
+
+typedef long long int ll;
+typedef double D;      // 座標値の型。doubleかlong doubleを想定
+typedef complex<D> P;  // Point
+typedef pair<P, P> L;  // Line
+typedef vector<P> VP;
+
 
 const int INF = (1<<30) - 1;
+const D EPS = 1e-9;
+
+#define X real()
+#define Y imag()
+#define LE(n,m) ((n) < (m) + EPS)
+#define GE(n,m) ((n) + EPS > (m))
+#define EQ(n,m) (abs((n)-(m)) < EPS)
+
 
 
 int main(){
