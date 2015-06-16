a52b7a324fd1af23e4a85993a10edc192bd0b19c
aki33524 <aki33524@aki33524-no-MacBook-Air.local>
2015-06-16 21:15:10 +0900

    二次元累積の調整

--- src/miscs/imos.cpp
+++ src/miscs/imos.cpp
@@ -1,22 +1,30 @@
 #include "../template.cpp"
 
+/*
+ * 二次元累積和
+ * 内部的には1-indexedで累積和を保持するが
+ * 実際にareaで累積和を取得する時は終端開区間の0-indexed
+ */
+
 class Imos{
 public:
 	int H, W;
+
+	//1-indexed
 	vector<vector<int>> d;
 public:
-	//1-indexed
-	Imos(vector<string>& field){
+	Imos(vector<vector<int>>& field){
 		H = field.size();
 		W = field[0].size();
-		d = vector<vector<int> >(H+1, vector<int>(W+1));
+		d = vector<vector<int>>(H+1, vector<int>(W+1));
 
 		for(int i=0; i<H; i++){
 			for(int j=0; j<W; j++){
-				d[i+1][j+1] = d[i+1][j] + d[i][j+1] - d[i][j] + field[i][j]-'0';
+				d[i+1][j+1] = d[i+1][j] + d[i][j+1] - d[i][j] + field[i][j];
 			}
 		}
 	}
+	//0-indexed
 	int area(int h1, int w1, int h2, int w2){
 		//size of area s.t. [h1, h2) and [w1, w2)
 		return d[h2][w2] - d[h2][w1] - d[h1][w2] + d[h1][w1];
